00b7224bfddb3e07cb8963046cd1b49b004c6751
Peter <yancong.li@alumni.ubc.ca>
2019-01-01 21:57:48 -0800

    scoreKeeper

---
+++ scoreKeeper.js
@@ -0,0 +1,54 @@
+let p1Button = document.querySelector("#p1");
+let p2Button = document.getElementById("p2");
+let resetButton = document.getElementById('reset');
+let p1Score = 0;
+let p2Score = 0;
+let p1Display = document.querySelector("#p1Display");
+let p2Display = document.getElementById("p2Display");
+let gameOver = false;
+let numInput = document.querySelector('input');
+let p = document.querySelector('p');
+let winningScoreDisplay = document.querySelector('p span');
+let winningScore = 5;
+
+p1Button.addEventListener("click", function () {
+	if (!gameOver) {
+		p1Score++;
+		if (p1Score === winningScore) {
+			p1Display.classList.add('winner');
+			gameOver = true;
+		}
+		p1Display.textContent = p1Score;
+	}
+});
+
+p2Button.addEventListener("click", function () {
+	if (!gameOver) {
+		p2Score++;
+		p2Display.textContent = p2Score;
+		if (p2Score === winningScore) {
+			p2Display.classList.add('winner');
+			gameOver = true;
+		}
+	}
+});
+
+resetButton.addEventListener("click", function () {
+	resetGame();
+});
+
+function resetGame() {
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove('winner');
+	p2Display.classList.remove('winner');
+	gameOver = false;
+}
+
+numInput.addEventListener('change', function() {
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	resetGame();
+});
