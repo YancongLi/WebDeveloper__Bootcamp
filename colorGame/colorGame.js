6ed9af231fd95d060182913b67e2e4b5b4035787
Peter <yancong.li@alumni.ubc.ca>
2019-01-03 22:17:06 -0800

    Refactor colorGame

--- colorGame/colorGame.js
+++ colorGame/colorGame.js
@@ -1,80 +1,69 @@
 let numOfSquares = 6;
-let colors = generateRandomColors(numOfSquares);
+let colors = [];
+let pickedColor;
 let squares = document.querySelectorAll('.square');
-let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
 let messageDisplay = document.getElementById('message')
 let h1 = document.querySelector('h1');
 let resetButton = document.querySelector('#reset');
-let easyBtn = document.querySelector('#easyBtn');
-let hardBtn = document.querySelector('#hardBtn');
+let modeBtns = document.querySelectorAll('.mode');
 
-easyBtn.addEventListener('click', function () {
-	this.classList.add('selected');
-	hardBtn.classList.remove('selected');
-	numOfSquares = 3;
+init();
+
+function init () {
+	setupModeButtons();
+	setupSquareListeners();
+	resetColors();
+	resetButton.addEventListener('click', function () {
+		resetColors();
+	});
+}
+
+function setupModeButtons () {
+	for(let i = 0; i < modeBtns.length; i++) {
+		modeBtns[i].addEventListener("click", function () {
+			modeBtns.forEach(function (btn) {
+				btn.classList.remove('selected');
+			});
+			this.classList.add('selected');
+			this.textContent === 'Easy' ? numOfSquares = 3 : numOfSquares = 6;
+			resetColors();
+		});
+	}
+}
+
+function setupSquareListeners () {
+	for (let i = 0; i < squares.length; i++) {
+		squares[i].addEventListener('click', function () {
+			let clickedColor = this.style.backgroundColor;
+			if (clickedColor === pickedColor) {
+				messageDisplay.textContent = 'Correct!';
+				resetButton.textContent = 'Play Again?';
+				changeColors(pickedColor);
+				h1.style.background = pickedColor;
+			}else {
+				this.style.backgroundColor = '#232323';
+				messageDisplay.textContent = 'Try Again!'
+			}
+		});
+	}
+}
+
+function resetColors() {
 	colors = generateRandomColors(numOfSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = 'New Colors';
+	messageDisplay.textContent = '';
 	for (let i = 0; i < squares.length; i++) {
 		if (colors[i]) {
+			squares[i].style.display = 'block';
 			squares[i].style.backgroundColor = colors[i];
-		} else {
+		}else {
 			squares[i].style.display = 'none';
 		}
 	}
 	h1.style.backgroundColor = 'steelblue';
-	messageDisplay.textContent = '';
-});
-
-hardBtn.addEventListener('click', function () {
-	this.classList.add('selected');
-	easyBtn.classList.remove('selected');
-	numOfSquares = 6;
-	colors = generateRandomColors(numOfSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	for (let i = 0; i < squares.length; i++) {
-		squares[i].style.backgroundColor = colors[i];
-		squares[i].style.display = 'block';
-	}
-	h1.style.backgroundColor = 'steelblue';
-	messageDisplay.textContent = '';
-});
-
-resetButton.addEventListener('click', function () {
-	colors = generateRandomColors(numOfSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor;
-	//Change colors to squares
-	for (let i = 0; i < squares.length; i++) {
-		squares[i].style.backgroundColor = colors[i];
-	}
-	h1.style.backgroundColor = 'steelblue';
-	messageDisplay.textContent = '';
-	this.textContent = 'New Colors';
-});
-
-colorDisplay.textContent = pickedColor;
-for (let i = 0; i < squares.length; i++) {
-	//Add initial colors to squares
-	squares[i].style.backgroundColor = colors[i];
-
-	//Add click listeners to squares
-	squares[i].addEventListener('click', function () {
-		//Grab color of the clicked square
-		let clickedColor = this.style.backgroundColor;
-		//Compare color to pickedColor
-		if (clickedColor === pickedColor) {
-			messageDisplay.textContent = 'Correct!';
-			resetButton.textContent = 'Play Again?';
-			changeColors(pickedColor);
-			h1.style.background = pickedColor;
-		}else {
-			this.style.backgroundColor = '#232323';
-			messageDisplay.textContent = 'Try Again!'
-		}
-	});
 }
 
 function changeColors(color) {
@@ -102,8 +91,4 @@ function randomColor() {
 	let rgb = `rgb(${r}, ${g}, ${b})`;
 	return rgb;
 }
-
-
-
-
 
